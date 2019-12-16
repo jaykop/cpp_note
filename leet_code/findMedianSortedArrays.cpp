6f22b177c8eb802e92a9aa37e915dc02dd96ae35
Jay Jeong <jaykop.jy@gmail.com>
2019-12-16 02:44:37 -0800

    finished findMedianSortedArrays

--- leet_code/findMedianSortedArrays.cpp
+++ leet_code/findMedianSortedArrays.cpp
@@ -10,94 +10,27 @@ public:
         // get size of both vectors
         int s1 = (int)nums1.size();
         int s2 = (int)nums2.size();
+        int i = 0, j = 0;
+        
+        for (; i < s1 && j < s2;) {
 
-        // get iterations
-        int loop_end = s1 < s2 ? s1 : s2;
-        int i = 0;
-        for (; i < loop_end; i++) {
-
-            // need to consider!
-            // 1. pending values -> need to save in the waring list
-            // 2. index issue
-            int a = nums1[i];
-            int b = nums2[i];
-
-            // case when there is pending numbers
-            if (!wating_list.empty()) {
-
-                int c = wating_list.back();
-                wating_list.pop_back();
-
-                merged.push_back(min(min(a, b), c)); // add smallest to the list
-                
-                int largest = max(max(a, b), c);
-
-                while (!wating_list.empty() && 
-                    largest > wating_list.back()) {
-
-                    merged.push_back(wating_list.back());
-                    wating_list.pop_back();
-                }
-
-                wating_list.push_back(largest); // add max to the pending list
-
-                // add middle to the pending list too
-                // Checking for b
-                if ((a <= b && b <= c) || (c <= b && b <= a)) wating_list.push_back(b);
-
-                // Checking for a
-                else if ((b <= a && a <= c) || (c <= a && a <= b)) wating_list.push_back(a);
-
-                else wating_list.push_back(c);
-
+            if (nums1[i] > nums2[j]){
+                merged.push_back(nums2[j++]);
             }
 
-            // 
             else {
-                merged.push_back(min(a, b));
-                wating_list.push_back(max(a, b));
+                merged.push_back(nums1[i++]);
             }
         }
 
-        int rest_loop = s1 > s2 ? s1 : s2;
-        std::vector<int>& rests = nums1.size() > nums2.size() ? nums1 : nums2;
-        for (; i < rest_loop; i++) {
+        while (s1 > i) {
 
-            // need to consider!
-           // 1. pending values -> need to save in the waring list
-           // 2. index issue
-            int a = rests[i];
-
-            // case when there is pending numbers
-            if (!wating_list.empty()) {
-
-                int b = wating_list.back();
-                wating_list.pop_back();
-                int smallest = min(a, b);
-
-                while (!wating_list.empty() && 
-                    smallest > wating_list.back()) {
-
-                    merged.push_back(wating_list.back());
-                    wating_list.pop_back();
-                }
-
-                merged.push_back(smallest); // add smallest to the list
-
-                wating_list.push_back(max(a, b)); // add max to the pending list
-
-            }
-
-            // 
-            else
-                merged.push_back(a);
+                merged.push_back(nums1[i++]);
         }
 
-        // consume the numbers in the wating list
-        while (!wating_list.empty()) {
-
-            merged.push_back(wating_list.back());
-            wating_list.pop_back();
+        while (s2 > j) {
+            
+                merged.push_back(nums2[j++]);
         }
 
         // then eventually we get merged list
