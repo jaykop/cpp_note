bbf8fe5463dceaa4646aa9cfb8e0a0964618e19f
jaykop <jaykop.jy@gmail.com>
2019-12-17 11:37:19 -0800

    finished ZigZagConversion

--- leet_code/main.cpp
+++ leet_code/main.cpp
@@ -1,9 +1,9 @@
-#include "longestPalindrome.h"
+#include "ZigZagConversion.h"
 
 int main() {
 
 	Solution a;
-	auto result = a.longestPalindrome(std::string("bb"));
+	auto result = a.convert("ABC", 2);
 
 	return 0;
 }
