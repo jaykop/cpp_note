05bb66c97539993fd45c0830dd0431701f0cad84
Jay Jeong <jaykop.jy@gmail.com>
2020-05-10 17:55:25 -0700

    solved maxSatisfaction

--- leet_code/main.cpp
+++ leet_code/main.cpp
@@ -1,13 +1,10 @@
-#include "maxScoreWords.h"
+#include "ReducingDishes.h"
 
 int main() {
 
-	vector<string> words = { "dog", "cat", "dad", "good" };
-	vector<char> letters = { 'a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o' };
-	vector<int>	score = { 1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
+	vector<int> v = { -1,-8,0,5,-9 };
 	Solution a;
-	auto result = a.maxScoreWords(words, letters, score);
+	auto result = a.maxSatisfaction(v);
 
 	return 0;
 }
