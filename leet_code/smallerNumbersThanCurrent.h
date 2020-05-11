ac50c989976d0a3d4fa419a3326792667083bab2
jaykop <jaykop.jy@gmail.com>
2020-05-11 13:03:19 -0700

    added smallerNumbersThanCurrent a little better

--- leet_code/smallerNumbersThanCurrent.h
+++ leet_code/smallerNumbersThanCurrent.h
@@ -9,23 +9,40 @@ public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
 
         vector<int> toReturn(nums.size(), -1);
-        map<int, int> hashMap;
+        map<int, vector<int>> hashMap;
 
         int j = 0;
         for (const auto& i : nums) {
 
-            if (hashMap.find(i) == hashMap.end()) 
-                hashMap.insert({ i, j }); // value, index
+            if (hashMap.find(i) == hashMap.end()) {
+                vector<int> index;
+                index.emplace_back(j);
+                hashMap.insert({ i, index }); // value, index
+            }
+            else
+                hashMap[i].emplace_back(j);
+
             j++;
+
         }
 
         sort(nums.begin(), nums.end());
 
         j = 0;
+        int offset = 0;
         for (const auto& i : nums) {
 
-            toReturn[hashMap[i]] = j;
-            j++;
+            toReturn[hashMap[i].back()] = j;
+            hashMap[i].pop_back();
+
+            if (!hashMap[i].empty())
+                offset++;
+
+            else {
+                j += offset;
+                offset = 0;
+                j++;
+            }
         }
 
         return toReturn;
