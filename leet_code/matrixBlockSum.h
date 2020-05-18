d4813f6c851c68cdd68bc9ee80aff955107ff341
jaykop <jaykop.jy@gmail.com>
2020-05-18 12:28:52 -0700

    solved matrixBlockSum in better solution(prefix)

--- leet_code/matrixBlockSum.h
+++ leet_code/matrixBlockSum.h
@@ -30,3 +30,41 @@ public:
         return res;
     }
 };
+
+//class Solution {
+//public:
+//    int sizeX, sizeY;
+//    int getSum(int i, int j, vector<vector<int>>& sum)
+//    {
+//        if (i < 0 || j < 0) return 0;
+//        if (i >= sizeX) i = sizeX - 1;
+//        if (j >= sizeY) j = sizeY - 1;
+//        return sum[i][j];
+//    }
+//
+//    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int K) {
+//        sizeX = mat.size();
+//        sizeY = mat[0].size();
+//        int size = sizeX * sizeY;
+//        vector<vector<int>> sum(sizeX, vector<int>(sizeY, 0));
+//
+//        for (int it = 0; it < size; it++)
+//        {
+//            int i = it / sizeY;
+//            int j = it % sizeY;
+//            sum[i][j] = mat[i][j] + getSum(i - 1, j, sum) + getSum(i, j - 1, sum)
+//                - getSum(i - 1, j - 1, sum);
+//        }
+//
+//        vector<vector<int>> ans(sizeX, vector<int>(sizeY, 0));
+//        for (int it = 0; it < size; it++)
+//        {
+//            int i = it / sizeY;
+//            int j = it % sizeY;
+//            ans[i][j] = getSum(i + K, j + K, sum) + getSum(i - K - 1, j - K - 1, sum)
+//                - getSum(i - K - 1, j + K, sum) - getSum(i + K, j - K - 1, sum);
+//        }
+//
+//        return ans;
+//    }
+//};
