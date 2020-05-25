8782a2b53f1bf566b5d546b70ac6fd3768a510c6
Jay Jeong <jaykop.jy@gmail.com>
2020-05-25 16:28:02 -0700

    working on LRUCache

--- leet_code/LRUCache.h
+++ leet_code/LRUCache.h
@@ -2,50 +2,70 @@
 #include <list>
 #include <map>
 using namespace std;
-
 class LRUCache {
 public:
+
     list<int> order;
-    list<int>::iterator tail;
     map<int, int> table;
+    list<int>::iterator tail;
     int size;
 
-    void update_tail()
+    void update_tail(int key)
     {
-        auto new_tail = prev(tail);
-        table.erase(*tail);
-        order.erase(tail);
-        new_tail = tail;
+        if (order.size() > 1)
+        {
+            for (auto it = order.begin(); it != order.end(); it++)
+            {
+                if (*it == key)
+                {
+                    if (it == tail)
+                    {
+                        auto new_tail = prev(tail);
+                        table.erase(*tail);
+                        order.erase(tail);
+                        tail = new_tail;
+                    }
+                    else
+                    {
+                        order.erase(it);
+                    }
+                    break;
+                }
+            }
+        }
     }
 
-    LRUCache(int capacity)
-    {
+    LRUCache(int capacity) {
         size = capacity;
     }
 
     int get(int key) {
 
-        update_tail();
-
         auto found = table.find(key);
-        if (found == table.end()) return -1;
+        if (found == table.end())
+            return -1;
+
+        update_tail(key);
+
+        // cycle to the front
+        order.insert(order.begin(), key);
         return found->second;
     }
 
     void put(int key, int value) {
 
-        if (order.size() < size) {
-            if (order.empty())
-                tail = order.insert( order.begin(), key );
-            else
-                order.insert(order.begin(), key);
-            table[key] = value;
-        }
+        if (order.empty())
+            tail = order.insert(order.begin(), key);
         else
         {
-            update_tail();
-            order.insert(order.begin(), key);
+            if (order.size() < size)
+            {
+                update_tail(key);
+                order.insert(order.begin(), key);
+            }
         }
+
+        table[key] = value;
     }
 };
 
--- leet_code/main.cpp
+++ leet_code/main.cpp
@@ -1,5 +1,5 @@
 #include "LRUCache.h"
-
+#include <iostream>
 int main() {
 
 	//vector<int> v =	{ 2, 3, 4, 5, 18, 17, 6 };
@@ -10,13 +10,19 @@ int main() {
 
 	cache.put(1, 1);
 	cache.put(2, 2);
-	cache.get(1);       // returns 1
+	std::cout << cache.get(1) << "\n";       // returns 1
 	cache.put(3, 3);    // evicts key 2
-	cache.get(2);       // returns -1 (not found)
+	std::cout << cache.get(2) << "\n";       // returns -1 (not found)
 	cache.put(4, 4);    // evicts key 1
-	cache.get(1);       // returns -1 (not found)
-	cache.get(3);       // returns 3
-	cache.get(4);       // returns 4
+	std::cout << cache.get(1) << "\n";       // returns -1 (not found)
+	std::cout << cache.get(3) << "\n";       // returns 3
+	std::cout << cache.get(4) << "\n";       // returns 4
+
+	//cache.put(2, 1);
+	//std::cout << cache.get(2) << "\n";       // returns 1
+	//std::cout << cache.get(2) << "\n";       // returns -1 (not found)
+	//std::cout << cache.get(3) << "\n";       // returns -1 (not found)
+
 
 	return 0;
 }
