32163d89b455e404c2f845a36bba264164063251
jaykop <jaykop.jy@gmail.com>
2020-05-27 11:40:14 -0700

    still working on 3sum

--- leet_code/main.cpp
+++ leet_code/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 int main() {
 
-	vector<int> v =	{ -1, 0, 1, 2, -1, -4 };
+	vector<int> v = {1, -1, -1, 0};/*{ 0,0,0 };*//*{ 1, 2, -2, -1};*//*{ -1, 0, 1, 2, -1, -4 };*/
 	Solution a;
 	auto b = a.threeSum(v);
 
--- leet_code/threeSum.h
+++ leet_code/threeSum.h
@@ -1,45 +1,47 @@
 #pragma once
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+	vector<vector<int>> threeSum(vector<int>& nums) {
 
-        vector<vector<int>> res;
-        if (nums.size() < 3)
-            return res;
+		vector<vector<int>> res;
+		if (nums.size() < 3)
+			return res;
 
-        map<int, int> table;
-        for (int i = 0; i < nums.size(); i++)
-            table[i]++;
+		map<int, int> table;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size(); i++)
+			table[i]++;
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (table[i] > 0)
-            {
-                for (int j = i + 1; j < nums.size() - 1; j++)
-                {
-                    int third = -(nums[j] + nums[i]);
-                    auto found = find(nums.begin(), nums.end(), third);
-                    int k = distance(nums.begin(), found);
-                    if (found != nums.end()
-                        && i != k && j != k)
-                    {
-                        table[i]--;
-                        table[j]--;
-                        table[k]--;
-                        vector<int> sum;
-                        sum.push_back(nums[i]);
-                        sum.push_back(nums[j]);
-                        sum.push_back(third);
-                        res.push_back(sum);
-                    }
-                }
-            }
-        }
+		for (int i = 0; i < nums.size() - 1; i++)
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+			{
+				if (table[i] > 0)
+				{
+					int third = -(nums[j] + nums[i]);
+					auto found = find(nums.begin(), nums.end(), third);
+					int k = distance(nums.begin(), found);
+					if (found != nums.end()
+						&& i != k && j != k)
+					{
+						table[i]--;
+						table[j]--;
+						table[k]--;
+						vector<int> sum;
+						sum.push_back(nums[i]);
+						sum.push_back(nums[j]);
+						sum.push_back(third);
+						res.push_back(sum);
+					}
+				}
+			}
+		}
 
-        return res;
-    }
+		return res;
+	}
 };
