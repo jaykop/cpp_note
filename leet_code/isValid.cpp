ef1ce8932ebc2d17743dc3fbfee2d038dfbc4cf1
Jay Jeong <jaykop.jy@gmail.com>
2020-05-27 10:57:16 -0700

    better solution of isValid

--- leet_code/isValid.cpp
+++ leet_code/isValid.cpp
@@ -29,3 +29,22 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char& c : s) {
+            switch (c) {
+                case '(': 
+                case '{': 
+                case '[': paren.push(c); break;
+                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                default: ; // pass
+            }
+        }
+        return paren.empty() ;
+    }
+};
