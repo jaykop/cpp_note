bbab0ddf2638d92b396ab90ba216691d0556e17f
jaykop <jaykop.jy@gmail.com>
2020-07-06 13:42:31 -0700

    inorderTraversal using bfs

--- leet_code/inorderTraversal.h
+++ leet_code/inorderTraversal.h
@@ -1,30 +1,31 @@
 #pragma once
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    vector<int> ans;
-    void recursive(TreeNode* root)
-    {
-        TreeNode* left = root->left,
-            * right = root->right;
-        if (left) recursive(left);
-        ans.push_back(root->val);
-        if (right) recursive(right);
-    }
+    
     vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+    
+        stack<TreeNode*> s;
+        TreeNode* node = root;
+        
+        while (!s.empty() || node)
+        {
+            if (node)
+            {
+                s.push(node);
+                node = node->left;
+            }
+            
+            else
+            {
+                node = s.top();
+                ans.push_back(node->val);
+                s.pop();
+                node = node->right;
+            }
+        }
 
-        if (root) recursive(root);
         return ans;
     }
 };
