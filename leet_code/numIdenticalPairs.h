01c7a6477a3eb401cb264839eb9cf6d45187e848
jaykop <jaykop.jy@gmail.com>
2020-08-02 18:21:57 -0700

    updated numIdenticalPairs

--- leet_code/numIdenticalPairs.h
+++ leet_code/numIdenticalPairs.h
@@ -2,35 +2,23 @@
 #include <vector>
 #include <map>
 using namespace std;
-
 class Solution {
 public:
 
     int numIdenticalPairs(vector<int>& nums) {
 
         int ans = 0;
-        map<int, vector<int>> table; // val and index
+        map<int, int> table; // val and index
         int size = int(nums.size());
         for (int i = 0; i < size; ++i)
         {
-            auto found = table.find(nums[i]);
-            if (found == table.end())
-            {
-                vector<int> list;
-                list.push_back(i);
-                table.insert({ nums[i], list });
-            }
-            else
-            {
-                found->second.push_back(i);
-            }
+            table[nums[i]]++;
         }
 
         for (const auto& t : table)
         {
-            int t_size = int(t.second.size());
-            if (t_size > 1)
-                ans += t_size * (t_size - 1) / 2;
+            if (t.second > 1)
+                ans += t.second * (t.second - 1) / 2;
         }
 
         return ans;
