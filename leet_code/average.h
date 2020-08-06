cc48f75dbbdbd0dd0f58edc223b6709afb8e7dfa
jaykop <jaykop.jy@gmail.com>
2020-08-06 11:58:02 -0700

    average - with sort

--- leet_code/average.h
+++ leet_code/average.h
@@ -20,3 +20,16 @@ public:
         return (res - dmin - dmax) / (salary.size()-2);
     }
 };
+
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        
+        sort(salary.begin(), salary.end());
+        double res = 0;
+        for (int i = 1; i < salary.size()-1; ++i)
+            res += salary[i];
+        
+        return (res) / (salary.size()-2);
+    }
+};
