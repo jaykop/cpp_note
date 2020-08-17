0139735bbaf9470280b1df7d1cf95f4d267c3ad4
jaykop <jaykop.jy@gmail.com>
2020-08-16 17:48:08 -0700

    solved maxProfit with better solution

--- leet_code/maxProfit.h
+++ leet_code/maxProfit.h
@@ -2,20 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int profit = 0;
+        int profit = 0, minPrice = INT_MAX;
+        
         if (!prices.empty())
         {
-            size_t jSize = prices.size();
-            size_t iSize = jSize - 1;
-            
-            for (size_t i = 0; i < iSize; ++i)
+            for (size_t i = 0; i < prices.size(); ++i)
             {
-                for (size_t j = i + 1; j < jSize; ++j)
-                {
-                    int diff = prices[j] - prices[i];
-                    if (profit < diff)
-                        profit = diff;
-                }
+                if (prices[i] < minPrice)
+                    minPrice = prices[i];
+                else if (profit < prices[i] - minPrice)
+                    profit = prices[i] - minPrice;
             }
         }
         
