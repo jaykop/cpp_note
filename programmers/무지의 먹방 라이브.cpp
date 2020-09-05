57bd5ec9a7ba44422b8384fee787443850fc14c0
jaykop <jaykop.jy@gmail.com>
2020-09-05 13:50:38 +0900

    not solved 무지의 먹방 라이브

--- programmers/무지의 먹방 라이브.cpp
+++ programmers/무지의 먹방 라이브.cpp
@@ -1,29 +1,45 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 int solution(vector<int> food_times, long long k) {
 
     int i = 0, size = food_times.size();
+    map<int, int> next;
     while (k--)
     {
         int last = i;
         while (!food_times[i])
         {
-            i = ++i % size;
-            if (i == last) return -1;
+            if (next.find(i) == next.end())
+            {
+                while (!food_times[i])
+                {
+                    i = ++i % size;
+                    if (i == last)
+                        return -1;
+                }
+                next[last] = i;
+            }
+            else
+                i = next[last];
+
+            if (i == last) 
+                return -1;
         }
+
         food_times[i]--;
         i = ++i % size;
     }
 
-    return i + 1;
+    return (i % size) + 1;
 }
 
 int main()
 {
-    vector<int> v = { 3, 1, 2 };
-    solution(v, 5);
+    vector<int> v = { 2, 0, 1 };
+    solution(v, 2);
     return 0;
 }
